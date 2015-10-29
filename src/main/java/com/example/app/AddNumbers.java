46c22eed71ef853eb5e2a4edf5db832085654a6e
Karthik Ushkala <karthik1for3@gmail.com>
2015-10-29 03:00:54 +0000

    second comit

---
+++ src/main/java/com/example/app/AddNumbers.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+public class Add{
+	public static void main(String argv[]){
+		int arg1 = Integer.parseInt(argv[0]);
+		int arg2 = Integer.parseInt(argv[1]);
+		System.out.println("sum of " arg1 +" + " + arg2 + " = " + arg1 + arg2);
+	}
+}
